1d00dc13f8f544262d512696739be19c524ea7fe
Bo <Wen.yu@ontariotechu.net>
2020-09-21 17:25:27 -0400

    Turn List additional features
    +next turn function
    +add new turn function
    +random list generation on start

--- SandcastleScuffle/Assets/Studio Assets/Scripts/UI/UI_TurnListCard.cs
+++ SandcastleScuffle/Assets/Studio Assets/Scripts/UI/UI_TurnListCard.cs
@@ -6,7 +6,7 @@ using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum CardState
+public enum TurnState
 {
     CURRENT,
     NEXT,
@@ -14,10 +14,12 @@ public enum CardState
     END
 
 }
-public enum CardEvent
+public enum TurnEvent
 {
     PLAYER_MOVE,
+    PLAYER_PATHING,
     AI_MOVE,
+    AI_PATHING,
     ENVIROMENT,
     OTHER,
     END
@@ -28,8 +30,8 @@ public class UI_TurnListCard : MonoBehaviour
     //public vars
     //enum of different card types and effects
     [Header("Card Type")]
-    public CardState cardState;
-    public CardEvent cardEvent;
+    public TurnState turnState;
+    public TurnEvent turnEvent;
 
     //icon components and related sprites
     [Header("Icon Sprites")]
@@ -51,11 +53,16 @@ public class UI_TurnListCard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        turnState = TurnState.FUTURE;
+
         // get background component
         bgColour = GetComponent<Image>();
+        bgColour.color = new Color(0.0f, 0.0f, 0.0f);
 
         //get sprite component from icon object
         icon = iconComponent.GetComponent<Image>();
+        icon.sprite = eventIcons[0];
 
         //setup background color array
         stateBackgroundColours = new Color[3] { currentTurn, nextTurn, futureTurn };
@@ -65,35 +72,35 @@ public class UI_TurnListCard : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        CardUpdate();
+
     }
 
-    void CardUpdate()
+    //used to make sure that it runs the start function when instatiated 
+    private void Awake()
+    {
+        Start();
+    }
+
+
+    //update the card
+    public void CardUpdate()
     {
         //if the event enum is out of range then just return function.
+
         //this mostly used to avoid hitting the END enum (used like .length)
-        if ((int)cardState >= stateBackgroundColours.Length)
-        {
-            return;
-        }
-        else
+        if ((int)turnState < stateBackgroundColours.Length)
         {
             //set the backgorund colour base on the state chosen for the card.
-            bgColour.color = stateBackgroundColours[(int)cardState];
+            bgColour.color = stateBackgroundColours[(int)turnState];
         }
 
         // same use as the above
-        if ((int)cardEvent >= eventIcons.Length)
-        {
-            return;
-        }
-        else
+        if ((int)turnEvent < eventIcons.Length)
         {
             //set the icon of the card based on the event it represents
-            icon.sprite = eventIcons[(int)cardEvent];
+            icon.sprite = eventIcons[(int)turnEvent];
         }
     }
 }
